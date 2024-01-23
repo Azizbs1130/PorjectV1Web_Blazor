73e07d28bf195f7a8b05c6fddc8855317e98253d
Azizbs1130 <aziz.bensalah2018@gmail.com>
2024-01-23 21:52:04 +0100

    Updated Version

---
+++ ProjectV1Web_Models/Learnblazormodels/Demo_Productprop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1Web_Models.Learnblazormodels
+{
+    public class Demo_Productprop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ ProjectV1Web_Models/Learnblazormodels/Demo_Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1Web_Models.Learnblazormodels
+{
+    public class Demo_Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price {  get; set; }
+        public List<Demo_Productprop> ProductProperties { get; set; }
+
+    }
+}
