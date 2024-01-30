e6303f9ce96fd366556496960b28c3c538372cbe
Azizbs1130 <aziz.bensalah2018@gmail.com>
2024-01-30 02:50:27 +0100

    Implemented Connection to SQL and added 3 Projects to organize the work

---
+++ ProjectV1Web_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1Web_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<Category> Categories { get; set; }   /*this will create inside our database a table with the name of Categories*/
+
+    }
+}
